325ee137378230416fe2fbcd29ee75f3148ffa55
Erick Fonseca <erickboyfla@gmail.com>
2024-08-01 14:39:22 -0300

    first commit

---
+++ src/main/java/com/sgiep/sgiep_back/SgiepBackApplication.java
@@ -0,0 +1,13 @@
+package com.sgiep.sgiep_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgiepBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgiepBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgiep/sgiep_back/SgiepBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgiep.sgiep_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgiepBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
