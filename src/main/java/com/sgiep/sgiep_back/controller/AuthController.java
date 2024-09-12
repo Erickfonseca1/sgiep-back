6dd8c27166ddbb3ffae63e2cbb2d182c8e250918
Erick Fonseca <erick@inovenow.com>
2024-09-12 17:25:15 -0300

    allow cors

--- src/main/java/com/sgiep/sgiep_back/controller/AuthController.java
+++ src/main/java/com/sgiep/sgiep_back/controller/AuthController.java
@@ -6,13 +6,11 @@ import com.sgiep.sgiep_back.dto.ResponseDTO;
 import com.sgiep.sgiep_back.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "${cors.allowedOrigins}")
 public class AuthController {
 
     @Autowired
